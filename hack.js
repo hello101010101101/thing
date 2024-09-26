ee674711c787db4b12e72b7b153235d453586f5d
hello101010101101 <judah.eiger@heschel.com>
2024-09-26 14:05:45 -0700

    Update hack.js

--- hack.js
+++ hack.js
@@ -11,11 +11,3 @@ document.addEventListener("keydown", function (e) {
 			});
 	}
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.href !== 'https://blobby-boi.github.io/uBlobeBM/main.html') {
-        var script = document.createElement('script');
-        script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
-        document.body.appendChild(script);
-    }
-});
